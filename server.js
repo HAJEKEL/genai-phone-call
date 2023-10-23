e449776d2d9bcc85cfc4eb30945f764e93fca352
Henk <h.a.jekel@student.tudelft.nl>
2023-10-23 12:49:27 +0200

    added comments, looking for a way to add chatgpt

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-require("dotenv").config();
-const express = require("express");
-const ExpressWs = require("express-ws");
+require("dotenv").config();  //  loads configuration from a .env file into the environment using the dotenv package.
+const express = require("express"); // import express.js library to create express.js app
+const ExpressWs = require("express-ws"); // adds WebSocket support to an Express.js application.
 
 const { TextToSpeechService } = require("./tts-service");
 const { TranscriptionService } = require("./transcription-service");
 
-const app = express();
-ExpressWs(app);
+const app = express(); //express.js app instance
+ExpressWs(app); //add websocket support
 
-const PORT = 3000;
+const PORT = 3000; //set server port
 
-app.post("/incoming", (req, res) => {
+app.post("/incoming", (req, res) => { // Handle an incoming HTTP POST request at /incoming, it is the webhook for the phonecall
   res.status(200);
-  res.type("text/xml");
+  res.type("text/xml"); //xml response that sets up bi-directional streaming phone call with the app streamed at the url defined in .env
   res.end(`
   <Response>
     <Connect>
@@ -22,13 +22,13 @@ app.post("/incoming", (req, res) => {
   `);
 });
 
-app.ws("/connection", (ws, req) => {
-  ws.on("error", console.error);
+app.ws("/connection", (ws, req) => { // sets up a WebSocket route at /connection
+  ws.on("error", console.error); //event listener for the "error" event and calling built-in javascript console.error 
   // Filled in from start message
   let streamSid;
 
-  const transcriptionService = new TranscriptionService();
-  const ttsService = new TextToSpeechService({});
+  const transcriptionService = new TranscriptionService(); //initialize stt
+  const ttsService = new TextToSpeechService({}); //initialize tts
 
   // Incoming from MediaStream
   ws.on("message", function message(data) {
